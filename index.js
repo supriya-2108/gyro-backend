8f411196e890ce65695b372ae0e72b99e131b75f
supriya-2108 <supriyajha2108@gmail.com>
2025-01-25 09:22:05 +0530

    changes added

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
 import cors from "cors";
-import swaggerUi from "swagger-ui-express";
-import swaggerSpec from "./swaggerConfig.js";
-import YAML from "yamljs";
+import express from "express";
 import constants from "./src/config/constants.js";
 import database from "./src/config/database_connection.js";
-import userRouter from "./src/routes/live_dining_user.route.js";
-import userOperationRouter from "./src/routes/gyro_user_operation.route.js";
 import adminOperationRouter from "./src/routes/gyro_admin_operation.route.js";
+import userOperationRouter from "./src/routes/gyro_user_operation.route.js";
+import userRouter from "./src/routes/live_dining_user.route.js";
 import paymentRouter from "./src/routes/payment_operation.route.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-// const { YAML } = pkg;
-const CSS_URL =
-  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 const app = express();
 app.use(express.json());
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, "./src/static")));
 
 app.get("/", (req, res) => {
   res.send('Welcome to Gyro API! <a href="/api-docs">API Docs</a>');
@@ -37,7 +26,7 @@ app.listen(constants.port, async () => {
     await database;
     console.log("Database connection established successfully!");
   } catch (error) {
-    console.log(error.message);
+    console.log(error.message, "not connected");
   }
   console.log(
     "Server is listening on port" +
--- src/config/database_connection.js
+++ src/config/database_connection.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import constants from "./constants.js";
 mongoose.set("strictQuery", false);
+console.log(constants.dbConfig.dbUrl);
+
 const database = mongoose.connect(constants.dbConfig.dbUrl);
 
 export default database;
